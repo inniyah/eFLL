8e0cf3207296745b06d8a50741ce96416ce6122c
Miriam Ruiz <miriam@debian.org>
2021-03-17 15:50:04 +0100

    Build tests

--- tests/GeneralTest.cpp
+++ tests/GeneralTest.cpp
@@ -1,181 +1,183 @@
 #include <iostream>
 #include <fstream>
-#include "../Fuzzy.h"
+
+#include "../source/Fuzzy.h"
 
 using namespace std;
+using namespace eFLL;
 
 int main(int argc, char *argv[])
 {
-    Fuzzy *fuzzy = new Fuzzy();
+    auto fuzzy = std::make_shared<Fuzzy>();
 
     // FuzzyInput
-    FuzzyInput *temperature = new FuzzyInput(1);
+    auto temperature = std::make_shared<FuzzyInput>(1);
 
-    FuzzySet *veryLow = new FuzzySet(-5, -5, 0, 15);
+    auto veryLow = std::make_shared<FuzzySet>(-5, -5, 0, 15);
     temperature->addFuzzySet(veryLow);
-    FuzzySet *low = new FuzzySet(10, 20, 20, 30);
+    auto low = std::make_shared<FuzzySet>(10, 20, 20, 30);
     temperature->addFuzzySet(low);
-    FuzzySet *high = new FuzzySet(25, 30, 30, 35);
+    auto high = std::make_shared<FuzzySet>(25, 30, 30, 35);
     temperature->addFuzzySet(high);
-    FuzzySet *veryHigh = new FuzzySet(30, 45, 50, 50);
+    auto veryHigh = std::make_shared<FuzzySet>(30, 45, 50, 50);
     temperature->addFuzzySet(veryHigh);
 
     fuzzy->addFuzzyInput(temperature);
 
     // FuzzyInput
-    FuzzyInput *humidity = new FuzzyInput(2);
+    auto humidity = std::make_shared<FuzzyInput>(2);
 
-    FuzzySet *dry = new FuzzySet(-5, -5, 0, 30);
+    auto dry = std::make_shared<FuzzySet>(-5, -5, 0, 30);
     humidity->addFuzzySet(dry);
-    FuzzySet *comfortable = new FuzzySet(20, 35, 35, 50);
+    auto comfortable = std::make_shared<FuzzySet>(20, 35, 35, 50);
     humidity->addFuzzySet(comfortable);
-    FuzzySet *humid = new FuzzySet(40, 55, 55, 70);
+    auto humid = std::make_shared<FuzzySet>(40, 55, 55, 70);
     humidity->addFuzzySet(humid);
-    FuzzySet *sticky = new FuzzySet(60, 100, 105, 105);
+    auto sticky = std::make_shared<FuzzySet>(60, 100, 105, 105);
     humidity->addFuzzySet(sticky);
 
     fuzzy->addFuzzyInput(humidity);
 
     // FuzzyOutput
-    FuzzyOutput *speed = new FuzzyOutput(1);
+    auto speed = std::make_shared<FuzzyOutput>(1);
 
-    FuzzySet *off = new FuzzySet(0, 0, 0, 0);
+    auto off = std::make_shared<FuzzySet>(0, 0, 0, 0);
     speed->addFuzzySet(off);
-    FuzzySet *lowHumidity = new FuzzySet(30, 45, 45, 60);
+    auto lowHumidity = std::make_shared<FuzzySet>(30, 45, 45, 60);
     speed->addFuzzySet(lowHumidity);
-    FuzzySet *medium = new FuzzySet(50, 65, 65, 80);
+    auto medium = std::make_shared<FuzzySet>(50, 65, 65, 80);
     speed->addFuzzySet(medium);
-    FuzzySet *fast = new FuzzySet(70, 90, 95, 95);
+    auto fast = std::make_shared<FuzzySet>(70, 90, 95, 95);
     speed->addFuzzySet(fast);
 
     fuzzy->addFuzzyOutput(speed);
 
     // Building FuzzyRule
-    FuzzyRuleAntecedent *ifVeryLowAndDry = new FuzzyRuleAntecedent();
+    auto ifVeryLowAndDry = std::make_shared<FuzzyRuleAntecedent>();
     ifVeryLowAndDry->joinWithAND(veryLow, dry);
-    FuzzyRuleConsequent *thenOff1 = new FuzzyRuleConsequent();
+    auto thenOff1 = std::make_shared<FuzzyRuleConsequent>();
     thenOff1->addOutput(off);
-    FuzzyRule *fuzzyRule1 = new FuzzyRule(1, ifVeryLowAndDry, thenOff1);
+    auto fuzzyRule1 = std::make_shared<FuzzyRule>(1, ifVeryLowAndDry, thenOff1);
     fuzzy->addFuzzyRule(fuzzyRule1);
 
     // Building FuzzyRule
-    FuzzyRuleAntecedent *ifVeryLowAndComfortable = new FuzzyRuleAntecedent();
+    auto ifVeryLowAndComfortable = std::make_shared<FuzzyRuleAntecedent>();
     ifVeryLowAndComfortable->joinWithAND(veryLow, comfortable);
-    FuzzyRuleConsequent *thenOff2 = new FuzzyRuleConsequent();
+    auto thenOff2 = std::make_shared<FuzzyRuleConsequent>();
     thenOff2->addOutput(off);
-    FuzzyRule *fuzzyRule2 = new FuzzyRule(2, ifVeryLowAndComfortable, thenOff2);
+    auto fuzzyRule2 = std::make_shared<FuzzyRule>(2, ifVeryLowAndComfortable, thenOff2);
     fuzzy->addFuzzyRule(fuzzyRule2);
 
     // Building FuzzyRule
-    FuzzyRuleAntecedent *ifVeryLowAndHumid = new FuzzyRuleAntecedent();
+    auto ifVeryLowAndHumid = std::make_shared<FuzzyRuleAntecedent>();
     ifVeryLowAndHumid->joinWithAND(veryLow, humid);
-    FuzzyRuleConsequent *thenOff3 = new FuzzyRuleConsequent();
+    auto thenOff3 = std::make_shared<FuzzyRuleConsequent>();
     thenOff3->addOutput(off);
-    FuzzyRule *fuzzyRule3 = new FuzzyRule(3, ifVeryLowAndHumid, thenOff3);
+    auto fuzzyRule3 = std::make_shared<FuzzyRule>(3, ifVeryLowAndHumid, thenOff3);
     fuzzy->addFuzzyRule(fuzzyRule3);
 
     // Building FuzzyRule
-    FuzzyRuleAntecedent *ifVeryLowAndSticky = new FuzzyRuleAntecedent();
+    auto ifVeryLowAndSticky = std::make_shared<FuzzyRuleAntecedent>();
     ifVeryLowAndSticky->joinWithAND(veryLow, sticky);
-    FuzzyRuleConsequent *thenLow1 = new FuzzyRuleConsequent();
+    auto thenLow1 = std::make_shared<FuzzyRuleConsequent>();
     thenLow1->addOutput(lowHumidity);
-    FuzzyRule *fuzzyRule4 = new FuzzyRule(4, ifVeryLowAndSticky, thenLow1);
+    auto fuzzyRule4 = std::make_shared<FuzzyRule>(4, ifVeryLowAndSticky, thenLow1);
     fuzzy->addFuzzyRule(fuzzyRule4);
 
     // Building FuzzyRule
-    FuzzyRuleAntecedent *ifLowAndDry = new FuzzyRuleAntecedent();
+    auto ifLowAndDry = std::make_shared<FuzzyRuleAntecedent>();
     ifLowAndDry->joinWithAND(low, dry);
-    FuzzyRuleConsequent *thenOff4 = new FuzzyRuleConsequent();
+    auto thenOff4 = std::make_shared<FuzzyRuleConsequent>();
     thenOff4->addOutput(off);
-    FuzzyRule *fuzzyRule5 = new FuzzyRule(5, ifLowAndDry, thenOff4);
+    auto fuzzyRule5 = std::make_shared<FuzzyRule>(5, ifLowAndDry, thenOff4);
     fuzzy->addFuzzyRule(fuzzyRule5);
 
     // Building FuzzyRule
-    FuzzyRuleAntecedent *ifLowAndComfortable = new FuzzyRuleAntecedent();
+    auto ifLowAndComfortable = std::make_shared<FuzzyRuleAntecedent>();
     ifLowAndComfortable->joinWithAND(low, comfortable);
-    FuzzyRuleConsequent *thenOff5 = new FuzzyRuleConsequent();
+    auto thenOff5 = std::make_shared<FuzzyRuleConsequent>();
     thenOff5->addOutput(off);
-    FuzzyRule *fuzzyRule6 = new FuzzyRule(6, ifLowAndComfortable, thenOff5);
+    auto fuzzyRule6 = std::make_shared<FuzzyRule>(6, ifLowAndComfortable, thenOff5);
     fuzzy->addFuzzyRule(fuzzyRule6);
 
     // Building FuzzyRule
-    FuzzyRuleAntecedent *ifLowAndHumid = new FuzzyRuleAntecedent();
+    auto ifLowAndHumid = std::make_shared<FuzzyRuleAntecedent>();
     ifLowAndHumid->joinWithAND(low, humid);
-    FuzzyRuleConsequent *thenLow2 = new FuzzyRuleConsequent();
+    auto thenLow2 = std::make_shared<FuzzyRuleConsequent>();
     thenLow2->addOutput(lowHumidity);
-    FuzzyRule *fuzzyRule7 = new FuzzyRule(7, ifLowAndHumid, thenLow2);
+    auto fuzzyRule7 = std::make_shared<FuzzyRule>(7, ifLowAndHumid, thenLow2);
     fuzzy->addFuzzyRule(fuzzyRule7);
 
     // Building FuzzyRule
-    FuzzyRuleAntecedent *ifLowAndSticky = new FuzzyRuleAntecedent();
+    auto ifLowAndSticky = std::make_shared<FuzzyRuleAntecedent>();
     ifLowAndSticky->joinWithAND(low, sticky);
-    FuzzyRuleConsequent *thenMedium1 = new FuzzyRuleConsequent();
+    auto thenMedium1 = std::make_shared<FuzzyRuleConsequent>();
     thenMedium1->addOutput(medium);
-    FuzzyRule *fuzzyRule8 = new FuzzyRule(8, ifLowAndSticky, thenMedium1);
+    auto fuzzyRule8 = std::make_shared<FuzzyRule>(8, ifLowAndSticky, thenMedium1);
     fuzzy->addFuzzyRule(fuzzyRule8);
 
     // Building FuzzyRule
-    FuzzyRuleAntecedent *ifHighAndDry = new FuzzyRuleAntecedent();
+    auto ifHighAndDry = std::make_shared<FuzzyRuleAntecedent>();
     ifHighAndDry->joinWithAND(high, dry);
-    FuzzyRuleConsequent *thenLow3 = new FuzzyRuleConsequent();
+    auto thenLow3 = std::make_shared<FuzzyRuleConsequent>();
     thenLow3->addOutput(lowHumidity);
-    FuzzyRule *fuzzyRule9 = new FuzzyRule(9, ifHighAndDry, thenLow3);
+    auto fuzzyRule9 = std::make_shared<FuzzyRule>(9, ifHighAndDry, thenLow3);
     fuzzy->addFuzzyRule(fuzzyRule9);
 
     // Building FuzzyRule
-    FuzzyRuleAntecedent *ifHighAndComfortable = new FuzzyRuleAntecedent();
+    auto ifHighAndComfortable = std::make_shared<FuzzyRuleAntecedent>();
     ifHighAndComfortable->joinWithAND(high, comfortable);
-    FuzzyRuleConsequent *thenMedium2 = new FuzzyRuleConsequent();
+    auto thenMedium2 = std::make_shared<FuzzyRuleConsequent>();
     thenMedium2->addOutput(medium);
-    FuzzyRule *fuzzyRule10 = new FuzzyRule(10, ifHighAndComfortable, thenMedium2);
+    auto fuzzyRule10 = std::make_shared<FuzzyRule>(10, ifHighAndComfortable, thenMedium2);
     fuzzy->addFuzzyRule(fuzzyRule10);
 
     // Building FuzzyRule
-    FuzzyRuleAntecedent *ifHighAndHumid = new FuzzyRuleAntecedent();
+    auto ifHighAndHumid = std::make_shared<FuzzyRuleAntecedent>();
     ifHighAndHumid->joinWithAND(high, humid);
-    FuzzyRuleConsequent *thenFast1 = new FuzzyRuleConsequent();
+    auto thenFast1 = std::make_shared<FuzzyRuleConsequent>();
     thenFast1->addOutput(fast);
-    FuzzyRule *fuzzyRule11 = new FuzzyRule(11, ifHighAndHumid, thenFast1);
+    auto fuzzyRule11 = std::make_shared<FuzzyRule>(11, ifHighAndHumid, thenFast1);
     fuzzy->addFuzzyRule(fuzzyRule11);
 
     // Building FuzzyRule
-    FuzzyRuleAntecedent *ifHighAndSticky = new FuzzyRuleAntecedent();
+    auto ifHighAndSticky = std::make_shared<FuzzyRuleAntecedent>();
     ifHighAndSticky->joinWithAND(high, sticky);
-    FuzzyRuleConsequent *thenFast2 = new FuzzyRuleConsequent();
+    auto thenFast2 = std::make_shared<FuzzyRuleConsequent>();
     thenFast2->addOutput(fast);
-    FuzzyRule *fuzzyRule12 = new FuzzyRule(12, ifHighAndSticky, thenFast2);
+    auto fuzzyRule12 = std::make_shared<FuzzyRule>(12, ifHighAndSticky, thenFast2);
     fuzzy->addFuzzyRule(fuzzyRule12);
 
     // Building FuzzyRule
-    FuzzyRuleAntecedent *ifVeryHighAndDry = new FuzzyRuleAntecedent();
+    auto ifVeryHighAndDry = std::make_shared<FuzzyRuleAntecedent>();
     ifVeryHighAndDry->joinWithAND(veryHigh, dry);
-    FuzzyRuleConsequent *thenMedium3 = new FuzzyRuleConsequent();
+    auto thenMedium3 = std::make_shared<FuzzyRuleConsequent>();
     thenMedium3->addOutput(medium);
-    FuzzyRule *fuzzyRule13 = new FuzzyRule(13, ifVeryHighAndDry, thenMedium3);
+    auto fuzzyRule13 = std::make_shared<FuzzyRule>(13, ifVeryHighAndDry, thenMedium3);
     fuzzy->addFuzzyRule(fuzzyRule13);
 
     // Building FuzzyRule
-    FuzzyRuleAntecedent *ifVeryHighAndComfortable = new FuzzyRuleAntecedent();
+    auto ifVeryHighAndComfortable = std::make_shared<FuzzyRuleAntecedent>();
     ifVeryHighAndComfortable->joinWithAND(veryHigh, comfortable);
-    FuzzyRuleConsequent *thenFast3 = new FuzzyRuleConsequent();
+    auto thenFast3 = std::make_shared<FuzzyRuleConsequent>();
     thenFast3->addOutput(fast);
-    FuzzyRule *fuzzyRule14 = new FuzzyRule(14, ifVeryHighAndComfortable, thenFast3);
+    auto fuzzyRule14 = std::make_shared<FuzzyRule>(14, ifVeryHighAndComfortable, thenFast3);
     fuzzy->addFuzzyRule(fuzzyRule14);
 
     // Building FuzzyRule
-    FuzzyRuleAntecedent *ifVeryHighAndHumid = new FuzzyRuleAntecedent();
+    auto ifVeryHighAndHumid = std::make_shared<FuzzyRuleAntecedent>();
     ifVeryHighAndHumid->joinWithAND(veryHigh, humid);
-    FuzzyRuleConsequent *thenFast4 = new FuzzyRuleConsequent();
+    auto thenFast4 = std::make_shared<FuzzyRuleConsequent>();
     thenFast4->addOutput(fast);
-    FuzzyRule *fuzzyRule15 = new FuzzyRule(15, ifVeryHighAndHumid, thenFast4);
+    auto fuzzyRule15 = std::make_shared<FuzzyRule>(15, ifVeryHighAndHumid, thenFast4);
     fuzzy->addFuzzyRule(fuzzyRule15);
 
     // Building FuzzyRule
-    FuzzyRuleAntecedent *ifVeryHighAndSticky = new FuzzyRuleAntecedent();
+    auto ifVeryHighAndSticky = std::make_shared<FuzzyRuleAntecedent>();
     ifVeryHighAndSticky->joinWithAND(veryHigh, sticky);
-    FuzzyRuleConsequent *thenFast5 = new FuzzyRuleConsequent();
+    auto thenFast5 = std::make_shared<FuzzyRuleConsequent>();
     thenFast5->addOutput(fast);
-    FuzzyRule *fuzzyRule16 = new FuzzyRule(16, ifVeryHighAndSticky, thenFast5);
+    auto fuzzyRule16 = std::make_shared<FuzzyRule>(16, ifVeryHighAndSticky, thenFast5);
     fuzzy->addFuzzyRule(fuzzyRule16);
 
     // run it
